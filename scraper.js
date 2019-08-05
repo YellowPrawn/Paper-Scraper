8772e2b9fb23baa405e78c4e46e46e89d4a08a73
YellowPrawn <damienfung616@gmail.com>
2019-08-05 15:48:45 +0800

    feat: questions seperated into array elements

--- scraper.js
+++ scraper.js
@@ -1,13 +1,33 @@
 const fs = require('fs');
 const pdf = require('pdf-parse');
 
-module.exports convert = function(fileName){
-    let dataBuffer = fs.readFileSync(`././papers/${fileName}.pdf`);
+var sortedData;
+
+module.exports.convert = function(fileName){
+    let dataBuffer = fs.readFileSync(`./papers/${fileName}.pdf`);
 
     pdf(dataBuffer).then(function(data) {
-        fs.writeFile(`././papers/${fileName}.txt`, data.text, function (err) {
-            if (err) throw err;
+       	sortedData = [data.text];
+
+        var temp;
+
+        var i = 1;
+        while(true){
+        	const num = `\n${i.toString()}.`;
+        	if (sortedData[sortedData.length-1].includes(num)){
+        		temp = sortedData[sortedData.length-1].split(num);
+        		sortedData.pop();
+        		sortedData.push("\n"+temp[0]);
+        		sortedData.push("\n"+temp[1]);
+        	} else{
+        		fs.writeFile(`./papers/${fileName}.txt`, sortedData, function (err) {
+        			if (err) throw err;
+        		});
+        		
+        		break;
+        	}
+        	i++;
         }
-        );     
     });
 }
+
